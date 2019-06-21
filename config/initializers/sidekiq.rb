7f13cf9cdb3d43af09dc599b7caf90805a124a03
vaot <victorandrey_123@hotmail.com>
2019-06-21 00:01:15 -0700

    Config the client as well

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,9 @@ Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://redis:6379/0' }
   end
 end
+
+Sidekiq.configure_client do |config|
+  if Rails.env.production?
+    config.redis = { url: 'redis://redis:6379/0' }
+  end
+end
