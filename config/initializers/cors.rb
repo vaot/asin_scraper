b4c0f9488307bc9461858b5a646a6e57bf77ca91
vaot <victorandrey_123@hotmail.com>
2019-06-20 22:15:32 -0700

    Cors for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'localhost:4200', 'localhost:3001'
-    resource '*',
-      headers: :any,
-      methods: %i(get post put patch delete options head)
+  if Rails.env.development?
+    allow do
+      origins 'localhost:4200', 'localhost:3001'
+      resource '*',
+        headers: :any,
+        methods: %i(get post put patch delete options head)
+    end
+  end
+
+  if Rails.env.production?
+    allow do
+      origins 'victorvaot.com', 'www.victorvaot.com'
+      resource '*',
+        headers: :any,
+        methods: %i(get post put patch delete options head)
+    end
   end
 end
