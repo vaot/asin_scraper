77b1cc4392698b7ca98f011bfd9ea30d2b1a824f
vaot <victorandrey_123@hotmail.com>
2019-06-21 19:48:13 -0700

    Make these more robust.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 TITLES_EXPS = [
   "css('#productTitle').first.content.strip"
 ]
@@ -22,7 +15,7 @@ IMAGE_EXPS = [
 
 RANK_EXPS = [
   "css('#SalesRank').first.content.gsub('\n', '').match(/#[0-9]+/)[0].strip.gsub('#', '').to_i",
-  "at('th:contains('Best Sellers Rank')').parent.content.gsub('\n', '').match(/#[0-9]+/)[0].strip.gsub('#', '').to_i"
+  "at('th:contains(\"Best Sellers Rank\")').parent.content.gsub('\n', '').match(/#[0-9]+/)[0].strip.gsub('#', '').to_i"
 ]
 
 DIMENSIONS_EXPS = [
