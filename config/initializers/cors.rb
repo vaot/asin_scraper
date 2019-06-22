e3125fb1612ad398ad7827ee281f8ea2986ffe7c
vaot <victorandrey_123@hotmail.com>
2019-06-21 17:54:58 -0700

    Allow react app to talk to api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   if Rails.env.production?
     allow do
-      origins 'victorvaot.com', 'www.victorvaot.com'
+      origins 'victorvaot.com', 'www.victorvaot.com', 'react.victorvaot.com'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
