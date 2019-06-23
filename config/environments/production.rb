6f559558fd6697c018280566391fb42e70dc13d0
vaot <victorandrey_123@hotmail.com>
2019-06-22 20:08:02 -0700

    Break these up and include protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,11 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = ['www.victorvaot.com', 'victorvaot.com', 'react.victorvaot.com']
+  config.action_cable.allowed_request_origins = [
+    'http://www.victorvaot.com',
+    'http://victorvaot.com',
+    'http://react.victorvaot.com'
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
